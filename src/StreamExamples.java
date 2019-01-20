a81696690f5d7a94cd24a88a1492ac1932a9521e
LSchultebraucks <L.Schultebraucks@t-online.de>
2019-01-20 16:09:54 +0100

    added map and filter with method example

--- src/StreamExamples.java
+++ src/StreamExamples.java
@@ -4,6 +4,8 @@ import java.util.List;
 public class StreamExamples {
     public static void main(String[] args) {
         StreamExamples.filter();
+        StreamExamples.filterWithMethod();
+        StreamExamples.map();
     }
 
 
@@ -23,8 +25,34 @@ public class StreamExamples {
                 .forEach(System.out::println); // syntactic sugar for .forEach(word -> System.out.println(word));
     }
 
-    private static void map() {
+    private static void filterWithMethod() {
+        List<String> words = Arrays.asList("Abra", "Kadrabra", "Aloha");
 
+        // As Streams with calling a method in filter
+        words.stream()
+                .filter(StreamExamples::startsWithA)
+                .forEach(System.out::println); // syntactic sugar for .forEach(word -> System.out.println(word));
     }
+
+    private static boolean startsWithA(String word) {
+        return word.startsWith("A");
+    }
+
+    private static void map() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+
+        // Before
+        for (Integer number : numbers) {
+            int squaredNumber = number * number;
+            System.out.println(squaredNumber);
+        }
+
+        // As Streams
+        numbers.stream()
+                .map(number -> number * number)
+                .forEach(System.out::println);
+    }
+
+
 }
 
